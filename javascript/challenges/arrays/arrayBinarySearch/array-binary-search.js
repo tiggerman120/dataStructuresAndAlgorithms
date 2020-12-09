f3431f085dee66ea7b62c900e5b229dcebdc83c6
Garrett Cintron <grrttcintron@gmail.com>
2020-12-09 14:19:01 -0800

    js file updated

--- javascript/challenges/arrays/arrayBinarySearch/array-binary-search.js
+++ javascript/challenges/arrays/arrayBinarySearch/array-binary-search.js
@@ -1,11 +1,22 @@
 'use strict';
 
-function BinarySearch(sortedArray, key) {
-  for (let i = 0; i < sortedArray.length; i++) {
-    if (key === sortedArray[i]) {
-      return i;
+let sortedArray = [4, 8, 15, 16, 23, 42];
+
+
+function binarySearch(sortedArray, key) {
+  let L = 0;
+  let R = sortedArray.length - 1;
+  while (L <= R) {
+    var m = Math.floor((L + R) / 2);
+    if (sortedArray[m] < key) {
+      L = m + 1;
+    } else if (sortedArray[m] > key) {
+      R = m - 1;
     } else {
-      return -1;
+      return m;
     }
   }
+  return -1;
 }
+binarySearch(sortedArray, 43);
+binarySearch(sortedArray, 15);
