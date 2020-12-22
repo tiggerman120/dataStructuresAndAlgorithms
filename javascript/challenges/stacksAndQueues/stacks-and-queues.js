0e9fd93708f28c27bef570a085a4a6a78b0e8cdd
Garrett Cintron <grrttcintron@gmail.com>
2020-12-22 13:37:23 -0800

    classs 11 uml added

--- javascript/challenges/stacksAndQueues/stacks-and-queues.js
+++ javascript/challenges/stacksAndQueues/stacks-and-queues.js
@@ -8,77 +8,11 @@ class Stack {
   }
   push(value) {
     const node = new Node(value);//create a new node
-    node.next = this.top;//assign the top to the next node
+    node.top = this.top;//assign the top to the next node
     this.top = node;//assign the node to the top
-  }
-
-  pop() {
-    if (this.top === null) {
-      throw new Error('empty stack');
-    } else {
-      let temp = this.top;
-      this.top = this.top.next;
-      temp.next = null;
-      return temp.value;
-    }
-  }
-
-  peek() {
-    if (this.top === null) {
-      throw new Error('empty stack');
-    } else {
-      return this.top.value;
-    }
-  }
-
-  isEmpty() {
-    return this.top === null ? true : false;
-  }
-}
-
-class Queue {
-  constructor() {
-    this.front = null;
-  }
-  enqueue(value) {
-    let node = new Node(value);
-    if (this.front === null) {
-      return this.front = node;
-    }
-    let currentNode = this.front;
-    while (currentNode.next) {
-      currentNode = currentNode.next;
-    }
-    currentNode.next = node;
-    //node.next = this.front;
-    //this.front = node;
-
-  }
-
-  dequeue() {
-    if (this.front === null) {
-      throw new Error('empty queue');
-    }
-    let temp = this.front;
-    this.front = this.front.next;
-    temp.next = null;
-    return temp.value;
-  }
-
-  peek() {
-    if (this.front === null) {
-      throw new Error('empty queue');
-    } else {
-      return this.front.value;
-    }
-  }
-
-  isEmpty() {
-    return this.front === null ? true : false;
+    console.log(node);
   }
 }
 
 
-
-module.exports = { Stack, Queue };
-
+module.exports = Stack;
